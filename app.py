8deb8440f818dbc425bd319d4d934398860735c8
dolsen0 <derek.olsen@gmail.com>
2023-01-15 23:08:07 -0500

    cleaned text output and code

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from apiKey import apiPass
 
 openai.api_key = apiPass
 
-setting = input("what kind of setting would you like? Fantasy, futuristic, cyberpunk, etc")
+setting = input("\nwhat kind of setting would you like? Fantasy, futuristic, cyberpunk, etc\n")
 
 response = openai.Completion.create(
     model = "text-davinci-001",
     prompt = f"create a writing prompt based on a {setting} setting",
     temperature = .30,
-    max_tokens = 64,
+    max_tokens = 80,
     top_p = 1,
     frequency_penalty = 0,
     presence_penalty = 0,
